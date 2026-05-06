865db56eae3403369cf888808ad6eb9defcbb657
Gabriel R D <gabrielrduarte777@gmail.com>
2026-05-06 18:33:15 -0300

    Atualizando

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,59 @@
+import { supabaseAdmin } from "../config/supabase";
+
+/**
+ * @typedef {Object} RegisterResponse
+ * @property {boolean} sucess - Indica se a operação foi realizada com sucesso
+ * @property {Object} [User] - Os dados do usuário criado
+ * @property {string[] [errors]} - Lista de mensagens de erro
+ * @returns
+ */
+
+/**
+ * Controller para registro de novos usuários via Admin SDK
+ *
+ * @param {import('express').Request} req - Objeto de requisição
+ * @param {import('express').Response} res - Objeto de resposta
+ * @returns {Promise<Import('express').Response<RegisterResponse>>}
+ */
+export const registerUser = async (req, res) => {
+  /**
+   * @type {{email: string, password: string}}
+   */
+  const { email, password } = req.body;
+  const erros = [];
+
+  try {
+    if (!email) erros.push("O e-mail é obrigatório.");
+    if (!password || password.length < 6)
+      erros.push("A senha deve ter 6+ caracteres.");
+
+    /**
+     * Busca a lista de usuários para validar duplicidade
+     */
+    const {
+      data: { users },
+      error: listError,
+    } = await supabaseAdmin.auth.admin.listUsers();
+
+    if (users.some((u) => u.email === email)) {
+      erros.push("Este usuário já está cadastrado.");
+    }
+
+    if (erros.length > 0) {
+      return res.status(400).json({ success: false, errors: erros });
+    }
+
+    const { data, error: createError } =
+      await supabaseAdmin.auth.admin.createUser({
+        email,
+        password,
+        email_confirm: true,
+      });
+
+    if (createError) throw createError;
+
+    return res.status(201).json({ success: true, user: data.user });
+  } catch (err) {
+    return res.status(500).json({ errors: ["Erro interno no servidor."] });
+  }
+};
