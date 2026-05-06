82b7246fdeec2347ae61be46975e3aea645a9be1
Gabriel R D <gabrielrduarte777@gmail.com>
2026-05-06 18:33:15 -0300

    Atualizando

--- backend/rotas/rotaUsuario.js
+++
@@ -1,7 +0,0 @@
-const router = require("express");
-const rotaUsuario = router();
-const controleUsuario = require("../controles/controleUsuario");
-
-rotaUsuario.get("/", controleUsuario.GetListarUsuario);
-
-module.exports = rotaUsuario;
