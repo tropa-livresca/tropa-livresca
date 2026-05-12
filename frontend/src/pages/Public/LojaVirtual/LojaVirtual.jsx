e4db0affbbb21de1f49fb0ab2cb13ef760d76b04
Gabriel R D <gabrielrduarte777@gmail.com>
2026-05-12 11:46:34 -0300

    Teste e configuração de Routes

--- frontend/src/pages/Public/LojaVirtual/LojaVirtual.jsx
+++ frontend/src/pages/Public/LojaVirtual/LojaVirtual.jsx
@@ -1,7 +1,23 @@
-export default function LojaVirtual (){
-  return (
-    <div>
-        
-    </div>
-  )
+import { supabase } from "../../../lib/supabaseClient";
+import { useState, useEffect } from "react";
+
+/**
+ * Página da Loja Virtual
+ * 
+ * @returns {JSX.Element}
+ */
+export default function LojaVirtual() {
+  const [livro, setLivros] = useState([]);
+
+  useEffect(() => {
+    /**
+     * Método responsável por pegar os dados do Supabase  
+     */
+    const getLivros = async () => {
+      const { data } = await supabase.select("*").from("livros");
+      setLivros(data ?? null);
+    };
+  }, []);
+
+  return <div></div>;
 }
