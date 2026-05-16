94266f3e2bc7e738cbb8782da7904365c0258585
Gabriel R D <gabrielrduarte777@gmail.com>
2026-05-16 11:32:05 -0300

    Alterando as chaves para separação do backend

--- backend/src/config/supabase.js
+++ backend/src/config/supabase.js
@@ -1,13 +1,15 @@
-const { createClient } = require('@supabase/supabase-js');
+import {createClient} from "@supabase/supabase-js";
 
 /**
  * Cria um cliente para o servidor com as variáveis de .env
  * 
  * @type {import('@supabase/supabase-js').SupabaseClient}
  */
+console.log("URL do Supabase:", process.env.SUPABASE_URL); // Deve mostrar o link
+console.log("Chave carregada:", !!process.env.SUPABASE_SERVICE_ROLE_KEY); // Deve mostrar true
 const supabaseAdmin = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
-module.exports = {supabaseAdmin};
+export default supabaseAdmin;
