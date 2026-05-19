629467b2fa616bcc66a318d11492c03f47b0ea16
mat540p <mruydealmeida@gmail.com>
2026-05-19 16:21:46 +0000

    "aa"

---
+++ backend/src/controllers/autor.controller.js
@@ -0,0 +1,12 @@
+import supabase, { supabaseAdmin } from "../config/supabase.js";
+
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
+};
+
+export const GetAutor = async() =>{
+       return ( await supabase.from('livros').select());
+}
