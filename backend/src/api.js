ae6d5d5fce40c5521093107321ebf08f86dba536
luisfabiano77 <lfabianosilvaa@gmail.com>
2026-05-19 14:17:26 +0000

    mudança de caminho

--- backend/src/api.js
+++ backend/src/api.js
@@ -7,7 +7,8 @@ const app = express();
 
 // Configuração corrigida do CORS
 app.use(cors({
-    origin: "http://localhost:5173", credentials: true 
+    origin: "http://localhost:5173", credentials: true,
+    origin: "https://organic-orbit-x5x6v4wwjrxrh6jg-5173.app.github.dev/", credentials:true
 }));
 
 app.use(express.json());
