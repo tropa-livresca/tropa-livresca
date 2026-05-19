c454e2747b82efdab47cde62f24eb935bffbd712
GabrielRD05092008 <gabrielrduarte777@gmail.com>
2026-05-19 14:07:57 +0000

    Base

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = ["http://localhost:5173","https://ominous-waddle-7v5xpwvw7qwg2pg9v-3000.app.github.dev", "https://solid-space-eureka-r46pqvxg4666hw5.app.github.dev"];
 
 export const apiFetch = async (endpoint, options = {}) => {
   options.credentials = "include"; 
@@ -7,17 +7,17 @@ export const apiFetch = async (endpoint, options = {}) => {
     ...options.headers,
   };
 
-  let response = await fetch(`${BASE_URL}${endpoint}`, options);
+  let response = await fetch(`${BASE_URL[1]}${endpoint}`, options);
 
   if (response.status === 401 && endpoint !== "/api/auth/refresh") {
     try {
-      const refreshResponse = await fetch(`${BASE_URL}/api/auth/refresh`, {
+      const refreshResponse = await fetch(`${BASE_URL[1]}/api/auth/refresh`, {
         method: "POST",
         credentials: "include",
       });
 
       if (refreshResponse.ok) {
-        response = await fetch(`${BASE_URL}${endpoint}`, options);
+        response = await fetch(`${BASE_URL[1]}${endpoint}`, options);
       } else {
         window.location.href = "/login";
       }
