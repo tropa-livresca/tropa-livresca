42cf4b27607a9c9fda5610640d658b1d381b55a1
mat540p <mruydealmeida@gmail.com>
2026-05-26 16:49:57 +0000

    meus livros

---
+++ frontend/src/hooks/useLivros.js
@@ -0,0 +1,12 @@
+
+
+import { useContext } from "react";
+import { LivroContext } from "../context/auth/Livros";
+
+const useLivros = () =>{
+    const context = useContext(LivroContext);
+    return context;
+}
+
+export default useLivros;
+
