0b749ccdc5d3e1573843dd15e5aadd80b1ae3e58
mat540p <mruydealmeida@gmail.com>
2026-05-29 12:44:21 +0000

    mudanças na pagina meus livros e no arquivo livros.js

---
+++ frontend/src/services/Livros.js
@@ -0,0 +1,42 @@
+
+import { createContext, useState, useEffect } from "react";
+import { apiFetch } from "./api";
+
+    
+
+  export const GetLivros = async () =>{
+   try{
+     const res = await apiFetch("/api/livros/", {Method: "Get"});
+
+     const data = await res.json();
+     console.log(data);
+
+     if(!res.ok){
+      return data.error;
+     }
+
+     return data;
+
+   }catch{
+     return "error";
+   }
+}
+
+export const GetLivrosDeAutor = async () =>{
+   try{
+     const res = await apiFetch("/api/livros/", {Method: "Get"});
+
+     const data = await res.json();
+     console.log(data);
+
+     if(!res.ok){
+      return data.error;
+     }
+
+     return data;
+
+   }catch{
+     return "error";
+   }
+}
+
