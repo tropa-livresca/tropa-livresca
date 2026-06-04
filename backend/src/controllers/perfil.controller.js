56bdc2548d4e821958e3aa455d4e277ce0db5aaf
Gabriel R D <gabrielrduarte777@gmail.com>
2026-06-04 16:19:46 -0300

    Criadas as rotas de perfil, mas cuja configuração está por vir

--- backend/src/controllers/perfil.controller.js
+++ backend/src/controllers/perfil.controller.js
@@ -1,7 +1,11 @@
 import supabase from "../config/supabase.js"
 
 export const GetPerfil = async(req, res) =>{
-    const {data, error} = await supabase.from('users_profile').select("*").eq("id", supabase.auth.user().id);
+    const {data, error} = await supabase
+        .from('users_profile')
+        .select("*")
+        .eq("id", req.user.id)
+        .single();
     if (error) {
         return res.status(500).json({ error: error.message });
     }
@@ -9,8 +13,13 @@ export const GetPerfil = async(req, res) =>{
 }
 
 export const UpdatePerfil = async(req, res) =>{
-    const {nome, descricacao, imagem} = req.body;
-    const {data, error} = await supabase.from('users_profile').update({nome, descricacao, imagem}).eq("id", supabase.auth.user().id);
+    const {nome, telefone, descricao, imagem} = req.body;
+    const {data, error} = await supabase
+        .from('users_profile')
+        .update({nome, telefone, descricao, imagem})
+        .eq("id", req.user.id)
+        .select()
+        .single();
     if (error) {
         return res.status(500).json({ error: error.message });
     }
--- backend/src/routes/perfil.route.js
+++ backend/src/routes/perfil.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {GetPerfil} from "../controllers/perfil.controller.js";
 import {UpdatePerfil} from "../controllers/perfil.controller.js";
+import { checkAuth } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get('/', GetPerfil);
-router.put('/', UpdatePerfil);
+router.get('/', checkAuth, GetPerfil);
+router.put('/putPerfil', checkAuth, UpdatePerfil);
 
 export default router;
