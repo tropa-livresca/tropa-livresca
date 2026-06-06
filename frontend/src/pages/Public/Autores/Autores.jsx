4308cd8acf822cbf1523d8bf7a25f1c9d4f9d1cb
luisfabiano77 <lfabianosilvaa@gmail.com>
2026-06-06 00:25:05 +0000

    atualização navbar/geral

--- frontend/src/pages/Public/Autores/Autores.jsx
+++ frontend/src/pages/Public/Autores/Autores.jsx
@@ -1,13 +1,33 @@
 import styles from "./Autores.module.css";
 
-export default function Autores (){
+export default function Autores() {
   return (
-    <div>
-      <main>
-        <div className={styles.h1}>
-          <h1>Autores</h1>
+    <main>
+      <div className={styles.container}>
+        <div className={styles.subtitulo}>
+          <h4>
+            A editora Tropa Livresca reúne autores independentes de todo o
+            Brasil . Descubra escritores que compartilham histórias únicas,
+            cheias de emoção e originalidade em suas obras. Autopublicação com
+            qualidade editorial e reconhecimento!
+          </h4>
         </div>
-      </main>
-    </div>
-  )
+        <div className={styles.barrasearch}>
+          <input
+            type="text"
+            placeholder="Pesquisar..."
+            className={styles.inputsearch}
+          />
+          <button type="submit" className={styles.buttonsearch}>
+            🔍
+          </button>
+        </div>
+        <div className={styles.containerautores}>
+          <div className={styles.l1}></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </main>
+  );
 }
