a5e7730c1e19e9b2f448ec5e6ef0629c780867c6
mat540p <mruydealmeida@gmail.com>
2026-06-08 14:09:48 +0000

    problemas api

--- backend/src/controllers/livros.controller.js
+++ backend/src/controllers/livros.controller.js
@@ -5,9 +5,3 @@ export const GetLivros = async (req,res) => {
  console.log(data);
  return res.status(200).send(data);
 }
-
-export const GetLivrosDeAutor = async (req,res) => {
- const {data, error} = await supabase.from('livros').select().eq('fk_user_profile_id', req.params);
- console.log(data);
- return res.status(200).send(data);
-}
--- backend/src/routes/livros.route.js
+++ backend/src/routes/livros.route.js
@@ -5,6 +5,6 @@ import { GetLivros, GetLivrosDeAutor } from "../controllers/livros.controller.js
 const router = express.Router();
 
 router.get("/", GetLivros);
-router.get("/:autor", GetLivrosDeAutor);
+
 
 export default router;
