982598d73d135fd8fffa25ee84c2576bc965b637
Gabriel R D <gabrielrduarte777@gmail.com>
2026-06-10 18:27:14 -0300

    Atualizando os formulários de se autopublique com exceção de

--- frontend/src/pages/Private/NovoLivro/Conteudo/Conteudo.jsx
+++ frontend/src/pages/Private/NovoLivro/Conteudo/Conteudo.jsx
@@ -1,13 +1,45 @@
+import { useState, useEffect } from "react";
+import Input from "../../../../components/form/Input/Input";
 /**
  * Manuscrito (upload)
  * Capa do Livro (Dividir em frente, trás e orelhas)
  * Pré-visualização da capa
  */
+
+const [conteudo, setConteudo] = useState([]);
+const [capa, setCapa] = useState("");
+
 export default function Conteudo() {
   return (
     <main>
+      <h1>Contéudo</h1>
       <form>
-        Viva essa jossa!
+        <fieldset>
+          <label>
+            Subir arquivo do livro (Aceitamos formatos .pdf)
+            <Input
+              type="file"
+              value={conteudo}
+              handleOnChange={(e) => setConteudo(e.target.value)}
+            />
+          </label>
+        </fieldset>
+
+        <fieldset>
+          <label>
+            <Input
+              type="file"
+              value={capa}
+              handleOnChange={(e) => { setCapa(e.target.value) }}
+            />
+          </label>
+        </fieldset>
+
+        <div>
+          <button>Anterior</button>
+          <button>Posterior</button>
+        </div>
+        <button>Salvar</button>
       </form>
     </main>
   );
