78452c424468275b175aac8634f6b4a2a09a60f0
luisfabiano77 <lfabianosilvaa@gmail.com>
2026-08-09 22:30:38 +0000

    .

--- frontend/src/clients/features/autopublicacao/pages/Confirmacao/Confirmacao.jsx
+++ frontend/src/clients/features/autopublicacao/pages/Confirmacao/Confirmacao.jsx
@@ -1,4 +1,4 @@
-﻿import { useState, useEffect } from "react";
+﻿﻿import { useState, useEffect } from "react";
 import styles from "./Confirmacao.module.css";
 
 export default function Confirmacao({
