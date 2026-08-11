6d850c10707ab971496342c68fadc2984086dff5
Gabriel R D <gabrielrduarte777@gmail.com>
2026-08-10 22:10:46 -0300

    chore: Arrancando as dependências do material ui

--- frontend/src/admin/features/painelGeral/pages/BoasVindas/BoasVindas.jsx
+++ frontend/src/admin/features/painelGeral/pages/BoasVindas/BoasVindas.jsx
@@ -1,7 +1,3 @@
-import { Typography } from "@mui/material";
-
 export default function BoasVindas(){
-    return(<Typography variant="h4" component="h1" gutterBottom>
-        Bem-vindo(a) ao painel administrativo!
-    </Typography>);
+    return <>Bem-vindo(a) ao painel administrativo!</>;
 }
